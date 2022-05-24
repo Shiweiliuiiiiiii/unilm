4e000a67680d44c03a30cb746e0273c7d3b0bc6b
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-24 15:48:57 +0200

    add sparse hooks

--- beit/semantic_segmentation/configs/_base_/schedules/schedule_40k.py
+++ beit/semantic_segmentation/configs/_base_/schedules/schedule_40k.py
@@ -7,6 +7,4 @@ lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 runner = dict(type='IterBasedRunner', max_iters=40000)
 checkpoint_config = dict(by_epoch=False, interval=10000)
 evaluation = dict(interval=10001, metric='mIoU', priority='LOW')
-mask_hook = [
-    dict(type='ApplyMaskHook')
-]
+
