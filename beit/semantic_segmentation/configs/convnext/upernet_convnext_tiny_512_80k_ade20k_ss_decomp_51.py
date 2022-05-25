71b8625832fde6fc4ff707cc5db0923968184293
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-25 14:03:49 +0200

    change priority of evaluation

--- beit/semantic_segmentation/configs/convnext/upernet_convnext_tiny_512_80k_ade20k_ss_decomp_51.py
+++ beit/semantic_segmentation/configs/convnext/upernet_convnext_tiny_512_80k_ade20k_ss_decomp_51.py
@@ -8,7 +8,7 @@
 
 _base_ = [
     '../_base_/models/upernet_convnext.py', '../_base_/datasets/ade20k.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
 ]
 crop_size = (512, 512)
 
